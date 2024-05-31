93c3323e74273756647fb9972b86e3d11c9f1abe
raza-t580-wsl <beheshti110100@gmail.com>
2024-05-31 06:30:09 +0500

    Rewrite javascript code, Add calculation functionality
    - rewrote the whole javascript code, for cleaner code.
    - added a function parseInput() to parse the text input and obtain the
    operands and the operator.
    - added a function calculate() to operate the parsed input according to
    the operator.
    - added a function updateAll() to update the text input and variables.
    - added a function addHistory() to append the current operation to the
    history containers.

--- script.js
+++ script.js
@@ -1,55 +1,106 @@
 const input = document.querySelector('#input');
+const numericBtn = document.querySelectorAll('.button.numeric');
+const operatorBtn = document.querySelectorAll('.button.operator');
+const equalsBtn = document.querySelector('#equalbtn');
+const historyDiv = document.querySelector('#history');
 const historyWrapper = document.querySelector("#historywrapper");
-const historyContainer = document.querySelector("#history");
-const numericBtns = document.querySelectorAll(".button.numeric");
-const operatorBtns = document.querySelectorAll(".button.operation");
-const clearBtn = document.querySelector("#clearbtn");
-const bkspBtn = document.querySelector("#bckbtn");
-const MAXLENGTH = 20;
-const validChars = '0123456789.+-*/%';
-let holdTime = undefined;
+const allClearBtn = document.querySelector('#clearbtn');
+const bkspBtn = document.querySelector('#bckbtn');
+const NUMERICS = '0123456789.';
+const OPERATORS = '+-*/%';
+let backspacePressed = false;
+let num1 = null, num2 = null, operator = null, result = null, secondOp = null, opSymbol = null, isNum1Negative = false;
 
 historyWrapper.scrollTop = historyWrapper.scrollHeight;
 
-input.addEventListener('input', (e) => {
-    if (input.value.length <= MAXLENGTH){
-        input.value = filterInput(input.value);    
-    }
-    else {
-        input.value = input.value.slice(0, MAXLENGTH);
-    }
+input.addEventListener('keydown', e=> {
+    if (NUMERICS.includes(e.key)) numericBtn.forEach(i => i.value == e.key ? i.click() : null);
+    if (OPERATORS.includes(e.key)) operatorBtn.forEach(i => i.value == e.key ? i.click() : null);
+    e.key == 'Backspace' ? backspacePressed = true : backspacePressed = false;
+    e.key == '=' ? equalsBtn.click() : null;
+    e.key == 'Escape' ? allClearBtn.click() : null;
+    e.key == 'Enter' ? equalsBtn.click() : null;
+})
+input.addEventListener('input', ()=> {backspacePressed ? null : input.value = input.value.slice(0, -1)
 });
-input.addEventListener('keydown', (e) => {
-    switch(e.key)
-    {
-        case 'Escape':
-            clearInput();
-            break; 
-    }
-});
-numericBtns.forEach(i => i.addEventListener('click', (e) => input.value += e.target.innerText));
-operatorBtns.forEach(i => i.addEventListener('click', (e) => input.value += e.target.innerText));
-clearBtn.addEventListener('click',() => allClear());
-bkspBtn.addEventListener('click', () => input.value = input.value.slice(0, input.value.length -1));
-bkspBtn.addEventListener('mousedown', () => holdTime = setTimeout(clearInput, 1000));
-bkspBtn.addEventListener('mouseup', () => clearTimeout(holdTime));
-bkspBtn.addEventListener('mouseleave', () => {if (holdTime) clearTimeout(holdTime)});
+numericBtn.forEach(i => i.addEventListener('click', (e) => {
+    input.value += e.target.value;
+}));
+operatorBtn.forEach(i => i.addEventListener('click', (e) => {
+    input.value += e.target.value;
+}));
+equalsBtn.addEventListener('click', ()=>{
+    parseInput();
+    if (!isNaN(num1) && num1 !== null && !isNaN(num2) && num2 !== null){
+    result = calculate();
+    updateAll(result);
+}});
+allClearBtn.addEventListener('click', allClear);
+bkspBtn.addEventListener('click', e => input.value = input.value.slice(0, -1));
 
-function filterInput (value) {
-    let filteredValue = '';
-    for (let char of value)
+function calculate() {
+    switch(operator){
+        case '+':
+            opSymbol = '+';
+            return num1 + num2;
+            break;
+        case '-':
+            opSymbol = '−';
+            return num1 - num2;
+            break;
+        case '*':
+            opSymbol = '×';
+            return num1 * num2;
+            break;
+        case '/':
+            opSymbol = '÷';
+            return num1 / num2;
+            break;
+        case '%':
+            opSymbol = '%';
+            return num1 % num2;
+            break;
+    }
+}
+function parseInput() {
+    if (num1 == input.value) return;
+    num1 = null;
+    num2 = null;
+    for (let i = 0; i < input.value.length; i++)
         {
-            validChars.includes(char) ? filteredValue += char : null;
+            if (OPERATORS.includes(input.value[i])){
+                if(i == 0) continue;
+                operator = input.value[i];
+                num1 = parseFloat(input.value.slice(0, i));
+                num2 = parseFloat(input.value.slice(i + 1));
+                break;
+            }
         }
-    return filteredValue;
+    console.log(num1, operator, num2);
 }
 function allClear() {
-    historyContainer.innerHTML = '';
-    clearInput();
-}
-function clearInput() {
     input.value = '';
+    historyDiv.innerHTML = '';
+    num1 = null;
+    num2 = null;
+    operator = null;
 }
-function Backspace() {
-    input.value = input.value.slice(0, input.value.length);
+function updateAll(result) {
+    addHistory()
+    input.value = result;
+    num1 = result;
+    // console.log(num1);
+    if (secondOp){
+        operator = secondOp;
+        secondOp = null;
+    }
+}
+function addHistory(){
+    const historyItem = document.createElement('div');
+    historyItem.setAttribute('class', 'historyItem');
+    historyItem.innerText = `${num1} ${opSymbol} ${num2}`;
+    // console.log(`${num1} ${operator} ${num2}`)
+    // historyItem.innerText = input.value;
+    historyDiv.appendChild(historyItem);
+    historyWrapper.scrollTop = historyWrapper.scrollHeight;
 }
